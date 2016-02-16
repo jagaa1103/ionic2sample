ed55106f43ccdd78affc53675c354fb0296bbd9b
Jagaa <jagaa1103@gmail.com>
2016-02-16 16:43:01 +0100

    added list

--- ionic2sample/app/pages/page1/page1.js
+++ ionic2sample/app/pages/page1/page1.js
@@ -1,11 +1,10 @@
 import {Page} from 'ionic/ionic';
 
-
 @Page({
   templateUrl: 'build/pages/page1/page1.html'
 })
 export class Page1 {
-  constructor() {
-
-  }
+    constructor() {
+        // this.todos = ["hello", "world"];
+    }
 }
